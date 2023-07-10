35cf1cbfabf7a14373f76b78df5e98f07b042252
ElenaAnnibali <77542450+ElenaAnnibali@users.noreply.github.com>
2023-07-10 16:05:58 +0200

    Troubleshooting - 2 try

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Result from './Components/Result.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Result />
   </React.StrictMode>,
-)
+);

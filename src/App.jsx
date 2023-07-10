74b6a79587ff32f5cd8d92f5c41c55d7b9a79e24
ElenaAnnibali <77542450+ElenaAnnibali@users.noreply.github.com>
2023-07-10 16:22:18 +0200

    Troubleshooting - 3 try

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Result from './Components/Result';
 function App() {
   return (
     <>
+      <h1>Results summary component</h1>
       <Result />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Result from './Components/Result.jsx';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Result />
+    <App />
   </React.StrictMode>,
 );
